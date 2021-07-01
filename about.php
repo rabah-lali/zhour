6261cd77ade5f258b0a5a61580429177f6608642
rabah <rabah_lali@yahoo.com>
2021-07-01 16:12:47 +0100

    modified about.php

--- about.php
+++ about.php
@@ -3,6 +3,6 @@
  echo $header ;?>
  <div class="container">  
    <h3 align="center"><?= "Hello in the about page" ; ?></h3>        
-   <img src="images/about.png" alt="About"  width="80%" hight="30%">   
+   <img src="images/about.jpg" alt="About"  width="80%" hight="30%">   
 </div> 
 <?= $footer ; ?>
